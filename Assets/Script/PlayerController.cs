ec395c727730e9ca359fa66e7fcefabed1cf2c6d
rhm0202 <83155164+rhm0202@users.noreply.github.com>
2025-07-13 04:17:51 +0900

    .
    .

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
