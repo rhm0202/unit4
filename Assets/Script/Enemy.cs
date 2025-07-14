050dd39b6184c4a2b1b281970d5a15c70823bfbf
rhm0202 <83155164+rhm0202@users.noreply.github.com>
2025-07-14 21:46:39 +0900

    .
    .

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
